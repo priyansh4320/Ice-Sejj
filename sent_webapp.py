720817f1f6a7e98660af46cc5ace9b27cf03632c
Priyanshu Yashwant Deshmukh <69320370+priyansh4320@users.noreply.github.com>
2023-06-27 11:48:13 +0530

    Update sent_webapp.py

--- sent_webapp.py
+++ sent_webapp.py
@@ -76,7 +76,7 @@ def postag(text):
 #--------------------------------------------------------------------------------------------------------
 #app
 st.set_page_config(layout="wide")
-st.title("IceAge")
+st.title("Ice Age")
 st.write("A Streamlit Application That Demonstrates Power of Machine Learing.")
 
 with st.sidebar:
