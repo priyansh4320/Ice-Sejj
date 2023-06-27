5538766fb574c044c7cddfecd2b3b107f6e0b746
Priyanshu Yashwant Deshmukh <69320370+priyansh4320@users.noreply.github.com>
2023-06-27 11:42:44 +0530

    Update sent_webapp.py

--- sent_webapp.py
+++ sent_webapp.py
@@ -76,11 +76,11 @@ def postag(text):
 #--------------------------------------------------------------------------------------------------------
 #app
 st.set_page_config(layout="wide")
-st.title("Ice-Sejj")
+st.title("IceAge")
 st.write("A Streamlit Application That Demonstrates Power of Machine Learing.")
 
 with st.sidebar:
-    st.title("Ice-Sejj")
+    st.title("IceAge")
     st.header("Content Navigation")
 nav_selection = st.sidebar.radio('Navigation',("NLP","Classification","Regression","Description"))
 
@@ -410,19 +410,19 @@ if nav_selection=="Regression":
 if nav_selection=="Description":
     st.header("Project Description")
     st.write(f"""
-    Introducing Ice-Sejj: Your Ultimate NLP and Machine Learning Solution
+    Introducing IceAge: Your Ultimate NLP and Machine Learning Solution
 
-Ice-Sejj is a revolutionary project that brings together the power of Natural Language Processing (NLP) and machine learning, allowing you to seamlessly perform classification and regression tasks on any dataset. Designed for data scientists, researchers, and professionals alike, Ice-Sejj empowers you to unlock valuable insights and make data-driven decisions with ease.
+IceAge is a revolutionary project that brings together the power of Natural Language Processing (NLP) and machine learning, allowing you to seamlessly perform classification and regression tasks on any dataset. Designed for data scientists, researchers, and professionals alike, IceAge empowers you to unlock valuable insights and make data-driven decisions with ease.
 
-With Ice-Sejj's robust classification and regression capabilities, you can tackle a wide range of machine learning challenges. Whether you're predicting customer behavior, sentiment analysis, or financial trends, Ice-Sejj equips you with the tools to build accurate and reliable predictive models.
+With IceAge's robust classification and regression capabilities, you can tackle a wide range of machine learning challenges. Whether you're predicting customer behavior, sentiment analysis, or financial trends, IceAge equips you with the tools to build accurate and reliable predictive models.
 
-Gone are the days of grappling with complex data preprocessing and feature engineering. Ice-Sejj handles all the heavy lifting, automating data preprocessing, feature extraction, and model training. This allows you to focus on the most critical aspects of your analysis, interpretation, and decision-making.
+Gone are the days of grappling with complex data preprocessing and feature engineering. IceAge handles all the heavy lifting, automating data preprocessing, feature extraction, and model training. This allows you to focus on the most critical aspects of your analysis, interpretation, and decision-making.
 
-But Ice-Sejj doesn't stop at machine learning. It seamlessly integrates powerful NLP functionalities such as sentiment analysis, text summarization, Named Entity Recognition (NER), and POS tagging. This comprehensive NLP toolkit provides you with unparalleled capabilities to extract meaningful insights from text data.
+But IceAge doesn't stop at machine learning. It seamlessly integrates powerful NLP functionalities such as sentiment analysis, text summarization, Named Entity Recognition (NER), and POS tagging. This comprehensive NLP toolkit provides you with unparalleled capabilities to extract meaningful insights from text data.
 
-Ice-Sejj boasts a user-friendly interface built on the popular Streamlit framework, making it accessible and intuitive for users of all experience levels. Simply upload your dataset, select the desired classification or regression task, and let Ice-Sejj work its magic.
+IceAge boasts a user-friendly interface built on the popular Streamlit framework, making it accessible and intuitive for users of all experience levels. Simply upload your dataset, select the desired classification or regression task, and let IceAge work its magic.
 
-Discover the full potential of your data with Ice-Sejj. Harness the power of NLP and machine learning to solve complex problems, gain deeper understanding, and make informed decisions. Whether you're in academia, research, or industry, Ice-Sejj is your trusted companion on the journey to unlock the true value of your data.
+Discover the full potential of your data with IceAge. Harness the power of NLP and machine learning to solve complex problems, gain deeper understanding, and make informed decisions. Whether you're in academia, research, or industry, IceAge is your trusted companion on the journey to unlock the true value of your data.
 
 
 
